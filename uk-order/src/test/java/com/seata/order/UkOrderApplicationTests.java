672e85bb3efc7dd9e7647afbbcd86aad40f2b4b1
lee <leejiliang@126.com>
2023-05-26 15:09:57 +0800

    feat: init module uk-order

---
+++ uk-order/src/test/java/com/seata/order/UkOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seata.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UkOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
