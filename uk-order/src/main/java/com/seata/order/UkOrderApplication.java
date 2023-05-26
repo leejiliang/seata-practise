7520ad37358ada789a887d8eca7d35d279fd9d47
lee <leejiliang@126.com>
2023-05-26 16:15:29 +0800

    feat: 注册中心支持

--- uk-order/src/main/java/com/seata/order/UkOrderApplication.java
+++ uk-order/src/main/java/com/seata/order/UkOrderApplication.java
@@ -2,11 +2,15 @@ package com.seata.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableDiscoveryClient
 public class UkOrderApplication {
 
     public static void main(String[] args) {
@@ -17,5 +21,11 @@ public class UkOrderApplication {
     @GetMapping("/")
     public String helloUk() {
         return "hello order service";
+    }
+
+    @GetMapping("/my-health-check")
+    public ResponseEntity<String> myCustomCheck() {
+        String message = "Testing my healh check function";
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
