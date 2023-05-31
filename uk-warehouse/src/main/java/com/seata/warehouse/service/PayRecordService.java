5de51953f3f92fbabe48d8a3b714b8887d06d156
jia.you <jia.you@huifu.com>
2023-05-31 14:20:33 +0800

    编写mybatis-plus快速使用帮助文档

--- uk-warehouse/src/main/java/com/seata/warehouse/service/PayRecordService.java
+++ uk-warehouse/src/main/java/com/seata/warehouse/service/PayRecordService.java
@@ -4,6 +4,7 @@ import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.seata.warehouse.entity.PayRecord;
 import com.seata.warehouse.mapper.PayRecordMapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,17 +32,15 @@ public class PayRecordService {
      * 这里演示的是未使用seata时，事务不生效。不能加@Transactional注解，会导致数据源无法切换
      */
     @Transactional
+    @SneakyThrows
     public void testTransaction(PayRecord payRecord1, PayRecord payRecord2){
         PayRecordService payRecordService = (PayRecordService) AopContext.currentProxy();
 //        payRecordService.insertMaster1(payRecord1);
 //        payRecordService.insertMaster2(payRecord2);
-        CompletableFuture.supplyAsync(() -> payRecordService.query1()).whenComplete((payRecord, e) -> {
-            log.info("查询s1：{}", payRecord.getAccountId());
-        });
-        CompletableFuture.supplyAsync(() -> payRecordService.query2()).whenComplete((payRecord, e) -> {
-            log.info("查询s1：{}", payRecord.getAccountId());
-        });
+        CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> log.info("查询s1结果：" + payRecordService.query1().getAccountId()));
+        CompletableFuture<Void> future2 = CompletableFuture.runAsync(() -> log.info("查询s2结果：" + payRecordService.query2().getAccountId()));
 
+        CompletableFuture.allOf(future1, future2).join();
     }
 
     @DS("master") // 随机master
