4c2e93de5b0ad4642b363c2d6c688dc4669ba402
lee <leejiliang@126.com>
2023-05-31 11:09:29 +0800

    fix: 引入mybatisPlus后, 分布式事务失效, 手动设置其数据源为seata代理后的数据源即可

--- uk-order/src/main/java/com/seata/order/UkOrderApplication.java
+++ uk-order/src/main/java/com/seata/order/UkOrderApplication.java
@@ -22,7 +22,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@SpringBootApplication(exclude = SeataFeignClientAutoConfiguration.class)
+@SpringBootApplication
 @RestController
 @EnableDiscoveryClient
 @EnableFeignClients
