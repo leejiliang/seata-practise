b9a6d050ce9cb9ba259eed9df442e3062e85b64e
jia.you <jia.you@huifu.com>
2023-05-31 11:48:53 +0800

    warehouse集成mybatis-plus多数据源插件

---
+++ uk-warehouse/src/main/java/com/seata/warehouse/entity/PayRecord.java
@@ -0,0 +1,41 @@
+package com.seata.warehouse.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 
+ * @since 2023-05-31 10:56:31
+ */
+@Getter
+@Setter
+@TableName("pay_record")
+public class PayRecord implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    @TableField("account_id")
+    private Long accountId;
+
+    @TableField("pay_amount")
+    private BigDecimal payAmount;
+
+    @TableField("pay_time")
+    private LocalDateTime payTime;
+}
---
+++ uk-warehouse/src/main/java/com/seata/warehouse/mapper/PayRecordMapper.java
@@ -0,0 +1,16 @@
+package com.seata.warehouse.mapper;
+
+import com.seata.warehouse.entity.PayRecord;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 
+ * @since 2023-05-31 10:56:31
+ */
+public interface PayRecordMapper extends BaseMapper<PayRecord> {
+
+}
